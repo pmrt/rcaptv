b772e3ecfec4f41ae5b77390d8be6ef36e99e153
Pedro Martínez Martínez <pedrojmrt@gmail.com>
2023-06-23 01:11:34 +0200

    fix lang db

--- rcaptv/database/postgres/migrations/0001_initial.up.sql
+++ rcaptv/database/postgres/migrations/0001_initial.up.sql
@@ -1,28 +1,6 @@
 BEGIN;
 
 DO $$ BEGIN
-  CREATE TYPE langISO31661 AS ENUM (
-    'aa','hy','or','ab','hz','os','af','id','pa','ak','ig','pl',
-    'am','ii','ps','an','ik','pt','ar','io','qu','as','is','rm',
-    'av','it','rn','ay','iu','ro','az','ja','ru','ba','jv','rw',
-    'be','ka','sa','bg','kg','sc','bh','ki','sd','bi','kj','se',
-    'bm','kk','sg','bn','kl','si','bo','km','sk','br','kn','sl',
-    'bs','ko','sm','ca','kr','sn','ce','ks','so','ch','ku','sq',
-    'co','kv','sr','cr','kw','ss','cs','ky','st','cv','lb','su',
-    'cy','lg','sv','da','li','sw','de','ln','ta','dv','lo','te',
-    'dz','lt','tg','ee','lu','th','el','lv','ti','en','mg','tk',
-    'es','mh','tl','et','mi','tn','eu','mk','to','fa','ml','tr',
-    'ff','mn','ts','fi','mr','tt','fj','ms','tw','fo','mt','ty',
-    'fr','my','ug','fy','na','uk','ga','nb','ur','gd','nd','uz',
-    'gl','ne','ve','gn','ng','vi','gu','nl','wa','gv','nn','wo',
-    'ha','no','xh','he','nr','yi','hi','nv','yo','ho','ny','za',
-    'hr','oc','zh','ht','oj','zu','hu','om','other'
-  );
-EXCEPTION
-  WHEN duplicate_object THEN null;
-END $$;
-
-DO $$ BEGIN
   CREATE TYPE broadcasterType AS ENUM ('partner', 'affiliate', 'none');
 EXCEPTION
   WHEN duplicate_object THEN null;
@@ -49,7 +27,7 @@ CREATE TABLE IF NOT EXISTS vods (
   created_at timestamp NOT NULL,
   published_at timestamp NOT NULL,
   duration_seconds int NOT NULL,
-  lang langISO31661 NOT NULL,
+  lang varchar NOT NULL,
   thumbnail_url text NOT NULL,
   title varchar NOT NULL,
   view_count int NOT NULL
@@ -65,7 +43,7 @@ CREATE TABLE IF NOT EXISTS clips (
   creator_name varchar NOT NULL,
   title varchar NOT NULL,
   game_id varchar,
-  lang langISO31661 NOT NULL,
+  lang varchar NOT NULL,
   thumbnail_url text NOT NULL,
   duration_seconds decimal(5, 2) NOT NULL,
   view_count int NOT NULL,
